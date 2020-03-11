8938f4f8a6ee85b8ac44ea2d52dbee50272166a9
prajakt <prajakt.shastry@gmail.com>
2020-03-10 20:09:05 -0400

    Added autocomplete

--- init.el
+++ init.el
@@ -41,7 +41,7 @@
     ("bf390ecb203806cbe351b966a88fc3036f3ff68cd2547db6ee3676e87327b311" default)))
  '(package-selected-packages
    (quote
-    (neotree ac-ispell ace-window company-quickhelp company-terraform centaur-tabs github-review anaconda-mode company company-ansible company-go company-restclient sr-speedbar spacemacs-theme sql-indent flycheck-yamllint doom-themes py-isort evil-collection multi-term k8s-mode treemacs-icons-dired treemacs-projectile treemacs-evil treemacs groovy-mode jenkins nord-theme frame-local ov s-buffer dash-functional dirtree exotica-theme flycheck-pycheckers flycheck flycheck-pyflakes all-the-icons smart-mode-line-atom-one-dark-theme smart-mode-line atom-one-dark-theme python-docstring powerline helm-projectile flycheck-mypy flymake-json helm-core helm ggtags material-theme importmagic exec-path-from-shell dockerfile-mode docker ranger markdown-mode projectile ag magit format-sql org-link-minor-mode yaml-mode elpy cyberpunk-theme)))
+    (auto-complete flymake-go flymake-go-staticcheck go-autocomplete go-impl go-imports go-mode go-playground ansible ansible-doc ansible-vault cliphist neotree ac-ispell ace-window company-quickhelp company-terraform centaur-tabs github-review anaconda-mode company company-ansible company-go company-restclient sr-speedbar spacemacs-theme sql-indent flycheck-yamllint doom-themes py-isort evil-collection multi-term k8s-mode treemacs-icons-dired treemacs-projectile treemacs-evil treemacs groovy-mode jenkins nord-theme frame-local ov s-buffer dash-functional dirtree exotica-theme flycheck-pycheckers flycheck flycheck-pyflakes all-the-icons smart-mode-line-atom-one-dark-theme smart-mode-line atom-one-dark-theme python-docstring powerline helm-projectile flycheck-mypy flymake-json helm-core helm ggtags material-theme importmagic exec-path-from-shell dockerfile-mode docker ranger markdown-mode projectile ag magit format-sql org-link-minor-mode yaml-mode elpy cyberpunk-theme)))
  '(terraform-indent-level 2))
 
 
@@ -88,92 +88,7 @@
 ;; Window CONFIGURATION
 ;; --------------------------------------
 (global-set-key (kbd "M-p") 'ace-window)
-
-
-;; TREEMACS CONFIGURATION
-;; --------------------------------------
-(require 'use-package)
-(use-package treemacs
-  :ensure t
-  :defer t
-  :init
-  (with-eval-after-load 'winum
-    (define-key winum-keymap (kbd "M-0") #'treemacs-select-window))
-  :config
-  (progn
-    (setq treemacs-collapse-dirs                 (if (executable-find "python3") 3 0)
-          treemacs-deferred-git-apply-delay      0.5
-          treemacs-display-in-side-window        t
-          treemacs-eldoc-display                 t
-          treemacs-file-event-delay              5000
-          treemacs-file-follow-delay             0.2
-          treemacs-follow-after-init             t
-          treemacs-git-command-pipe              ""
-          treemacs-goto-tag-strategy             'refetch-index
-          treemacs-indentation                   2
-          treemacs-indentation-string            " "
-          treemacs-is-never-other-window         nil
-          treemacs-max-git-entries               5000
-          treemacs-missing-project-action        'ask
-          treemacs-no-png-images                 nil
-          treemacs-no-delete-other-windows       t
-          treemacs-project-follow-cleanup        nil
-          treemacs-persist-file                  (expand-file-name ".cache/treemacs-persist" user-emacs-directory)
-          treemacs-recenter-distance             0.1
-          treemacs-recenter-after-file-follow    nil
-          treemacs-recenter-after-tag-follow     nil
-          treemacs-recenter-after-project-jump   'always
-          treemacs-recenter-after-project-expand 'on-distance
-          treemacs-show-cursor                   nil
-          treemacs-show-hidden-files             t
-          treemacs-silent-filewatch              nil
-          treemacs-silent-refresh                nil
-          treemacs-sorting                       'alphabetic-desc
-          treemacs-space-between-root-nodes      t
-          treemacs-tag-follow-cleanup            t
-          treemacs-tag-follow-delay              1.5
-          treemacs-width                         35)
-
-    ;; The default width and height of the icons is 22 pixels. If you are
-    ;; using a Hi-DPI display, uncomment this to double the icon size.
-    ;;(treemacs-resize-icons 44)
-
-    (treemacs-follow-mode t)
-    (treemacs-filewatch-mode t)
-    (treemacs-fringe-indicator-mode t)
-    (pcase (cons (not (null (executable-find "git")))
-                 (not (null (executable-find "python3"))))
-      (`(t . t)
-       (treemacs-git-mode 'deferred))
-      (`(t . _)
-       (treemacs-git-mode 'simple))))
-  :bind
-  (:map global-map
-        ("M-0"       . treemacs-select-window)
-        ("C-x t 1"   . treemacs-delete-other-windows)
-        ("C-x t t"   . treemacs)
-        ("C-x t B"   . treemacs-bookmark)
-        ("C-x t C-t" . treemacs-find-file)
-        ("C-x t M-t" . treemacs-find-tag)))
-
-(use-package treemacs-evil
-  :after treemacs evil
-  :ensure t)
-
-(use-package treemacs-projectile
-  :after treemacs projectile
-  :ensure t)
-
-(use-package treemacs-icons-dired
-  :after treemacs dired
-  :ensure t
-  :config (treemacs-icons-dired-mode))
-
-
-;; NEOTREE CONFIGURATION
-;; --------------------------------------
-(:map global-map
-      ("C-'" neotree-toggle))
+(global-set-key (kbd "C-'") 'neotree-toggle)
 
 
 ;; MAGIT CONFIGURATION
